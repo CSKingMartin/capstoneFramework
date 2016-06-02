554519700cdd70ce925f188364cc4a2352de39f7
CSKingMartin <cwells@linfield.edu>
2016-06-02 13:53:51 -0700

    cleaned up gulpfile.js, and added gulp-cssmin

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,22 @@
-var gulp = require('gulp');
+var gulp = require('gulp'); //required for gulp
 var nunjucksRender = require('gulp-nunjucks-render');
 var sass = require('gulp-sass')
 var sequence = require('run-sequence');
 var browserSync = require('browser-sync');
+var ccsmin = require('gulp-cssmin');
+var rename = require('gulp-rename');
 
 gulp.task('test', function(){
 	console.log('Congratulations! Gulp is working correctly!');
 });
 
-//does nunjucks
+//does nunjucks.
+//Nunjucks is the tool that takes the Nunjucks part of our HTML code
+//(the partials, modules, and blocks) and compiles them into a single
+//HTML document in the 'dist' directory for deployment.
 gulp.task('nunjucks', function() {
 
-	console.log('running nunjucks');
+	console.log('Compiling HTML');
 
 	return gulp.src('app/pages/**/*.html')
 	// render template with nunjucks
@@ -19,8 +24,9 @@ gulp.task('nunjucks', function() {
 		path: ['app/templates/','app/templates/partials/'],
 		data: { css_path: 'app/templates/style.css'}
 	}))
-	// output files in gulpTest
+	// output files in 'dist' directory
 	.pipe(gulp.dest('dist'))
+	//refresh the browser with updated HTML file
 	.pipe(browserSync.reload({
 		stream: true
 	}));
@@ -28,14 +34,26 @@ gulp.task('nunjucks', function() {
 
 });
 
-//does sass
+//compiles sass, then minify the css files.
+//SASS is developer-friendly formatted CSS, unreadable by web browwers.
+//gulp-sass takes these files, and creates a single master .css file
+//in the 'dist' directory for deployment.
+//Additionally, before the .css file is finished, our plugin gulp-cssmin
+//minifies the css as well.
 gulp.task('styles', function(){
 
-	console.log('running gulp-sass')
+	console.log('Compiling CSS')
 
 	return gulp.src('app/templates/sass/*.scss')
+		//compiles sass
 		.pipe(sass().on('error', sass.logError))
+		//minivfies css
+		.pipe(ccsmin())
+		//puts .min suffix
+		.pipe(rename({suffix:'.min'}))
+		//put in directory
 		.pipe(gulp.dest('dist'))
+		//refresh browser with changes
 		.pipe(browserSync.reload({
 			stream: true
 		}));
@@ -44,15 +62,16 @@ gulp.task('styles', function(){
 //default task
 gulp.task('default', function(done) {
 
-	console.log('running default')
+	//console.log('running default')
 
-	sequence('nunjucks', 'styles','browserSync', done);
+	sequence('nunjucks', 'styles', done);
 	
 });
 
 //watch task
-gulp.task('watch', ['default'], function(){
-	gulp.watch('app/templates/**/*.+(scss|html)',['default']);
+gulp.task('watch', ['default', 'browserSync'], function(){
+	gulp.watch('app/templates/**/*.+(scss|html)',['default'],
+	['browsersync']);
 	gulp.watch('app/pages/**/*.html', ['default'])
 });
 
