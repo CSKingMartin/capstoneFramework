422129de2a8afa6d4fd04c5db43dedfefd01d06a
CSKingMartin <cwells@linfield.edu>
2016-11-03 17:39:53 -0700

    keeping stuff for demo

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,15 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync');
+var handlebars = require('gulp-static-handlebars');
+var rename = require('gulp-rename');
+var sass = require('gulp-sass');
+
 
 gulp.task('default', function() {
 	console.log("Congratulations, Gulp is successfully working.");
 });
 
-gulp.task('build', function() {
-	gulp.src('src/*.**')
-	.pipe(gulp.dest('./dist/'));
-});
+gulp.task('build',['hb', 'sass']);
 
 gulp.task('watch', function() {
 	gulp.watch('src/**/*.*', function(){
@@ -24,4 +25,19 @@ gulp.task('bs',['watch'], function(){
 			baseDir: 'dist'
 		}
 	})
+});
+
+gulp.task('hb', function(){
+	gulp.src('./src/index.hbs')
+	    .pipe(handlebars({contents:"whatever"}, {
+	        partials: gulp.src('./src/components/**/*.hbs')
+	    }))
+	    .pipe(rename('index.html'))
+	    .pipe(gulp.dest('./dist'));
+});
+	
+gulp.task('sass', function() {
+	gulp.src('./src/main.scss')
+	.pipe(sass())
+	.pipe(gulp.dest('./dist'))
 });
